2324adb1769ab0de13c2d90e8f9f03d5c8b35e3a
SergSmith64 <sergsmith2008@yandex.ru>
2020-04-27 15:36:56 +0400

    fix

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 console.log("START");
 
-class Animal {
-    constructor(name) {
-      this.speed = 0;
-      this.name = name;
-    }
-    run(speed) {
-      this.speed = speed;
-      console.log(`${this.name} бежит со скоростью ${this.speed}.`);
-    }
-    stop() {
-      this.speed = 0;
-      console.log(`${this.name} стоит.`);
-    }
-  }
-  
-  let animal = new Animal("Мой питомец");
+let animal = {
+  eats: true,
+};
+let rabbit = {
+  jumps: true,
+};
+
+rabbit.__proto__ = animal;
+
+// class Animal {
+// 		constructor(name) {
+// 			this.speed = 0;
+// 			this.name = name;
+// 		}
+// 		run(speed) {
+// 			this.speed = speed;
+// 			console.log(`${this.name} бежит со скоростью ${this.speed}.`);
+// 		}
+// 		stop() {
+// 			this.speed = 0;
+// 			console.log(`${this.name} стоит.`);
+// 		}
+// 	}
+
+// 	let animal = new Animal("Мой питомец");
+
+console.log("END");
