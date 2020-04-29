fcb80eeaced4b06b0346c5565a4f52c6eb558093
SergSmith64 <sergsmith2008@yandex.ru>
2020-04-29 11:34:50 +0400

    fix: add temp files for output

---
+++ keyboard.js
@@ -0,0 +1,12 @@
+console.log("__START__");
+
+window.onload = function () {
+  // let screen = document.createElement('div');
+  // screen.className = 'wrap-screen';
+  // screen.innerHTML = '<textarea class="screen-txt" id="screen" rows="9" autofocus></textarea>';
+  // document.body.append(screen);
+
+  let wrapRow = document.createElement('div');
+  wrapRow.className = 'wrap-keyboard';
+  document.body.append(wrapRow);
+}
